a2962a24aad197cac1d3e9a00218956ed18ed814
Todd Erickson <twericks@usc.edu>
2020-06-05 11:08:31 -0700

    Got the basic recyclerview working

--- app/src/main/java/com/example/mvvm/MainActivity.java
+++ app/src/main/java/com/example/mvvm/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.mvvm.adapters.RecyclerAdapter;
+import com.example.mvvm.models.NicePlace;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private List<NicePlace> mNicePlaces = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started");
+
+        initNicePlaces();
+        initRecyclerView();
+    }
+
+    private void initNicePlaces() {
+        Log.d(TAG, "initNicePlaces: preparing nice places");
+
+        mNicePlaces.add(new NicePlace("Arizona","https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Flag_of_Arizona.svg/1920px-Flag_of_Arizona.svg.png"));
+        mNicePlaces.add(new NicePlace("Scottsdale", "https://upload.wikimedia.org/wikipedia/en/thumb/c/c6/Seal_of_Scottsdale_%28Arizona%29.svg/1024px-Seal_of_Scottsdale_%28Arizona%29.svg.png"));
+        mNicePlaces.add(new NicePlace("Tempe", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Tempe%2C_Arizona.svg/1280px-Flag_of_Tempe%2C_Arizona.svg.png"));
+        mNicePlaces.add(new NicePlace("Phoenix", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b1/Phoenix-logo.svg/1220px-Phoenix-logo.svg.png"));
+        mNicePlaces.add(new NicePlace("Payson", "https://learningfromdogs.files.wordpress.com/2010/04/payson.jpg"));
+        mNicePlaces.add(new NicePlace("Chandler", "https://upload.wikimedia.org/wikipedia/en/8/8d/Chandleraz_seal.png"));
+        mNicePlaces.add(new NicePlace("Gilbert", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Seal_of_Gilbert%2C_Arizona.svg/966px-Seal_of_Gilbert%2C_Arizona.svg.png"));
+        mNicePlaces.add(new NicePlace("Mesa", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Flag_of_Mesa%2C_Arizona.svg/1280px-Flag_of_Mesa%2C_Arizona.svg.png"));
+        mNicePlaces.add(new NicePlace("Fountain Hills", "https://upload.wikimedia.org/wikipedia/en/2/27/FountainHillsaz_seal.png"));
+        mNicePlaces.add(new NicePlace("Apache Junction", "https://upload.wikimedia.org/wikipedia/en/5/5f/Apache_junction_seal.png"));
+        mNicePlaces.add(new NicePlace("Surprise", "https://upload.wikimedia.org/wikipedia/en/7/76/Surprise_AZ_seal.png"));
+    }
+
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(this, mNicePlaces);
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/mvvm/adapters/RecyclerAdapter.java
@@ -0,0 +1,80 @@
+package com.example.mvvm.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mvvm.R;
+import com.example.mvvm.models.NicePlace;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    private static final String TAG = "RecyclerAdapter";
+    private List<NicePlace> mNicePlaces = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerAdapter(Context context, List<NicePlace> nicePlaces) {
+        super();
+        mContext = context;
+        mNicePlaces = nicePlaces;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_nice_place_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called on position " + position);
+        NicePlace nicePlace = mNicePlaces.get(position);
+
+        String title = nicePlace.getTitle();
+        holder.mLnpiTextView.setText(title);
+
+        String iconUrl = nicePlace.getImageUrl();
+        Context context = holder.mLnpiImageView.getContext();
+        Picasso.with(context).load(iconUrl).into(holder.mLnpiImageView);
+
+        holder.mLnpiCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "OnClick: clicked on: " + title);
+                Toast.makeText(mContext, title, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNicePlaces.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView mLnpiImageView;
+        TextView mLnpiTextView;
+        CardView mLnpiCardView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mLnpiImageView = itemView.findViewById(R.id.lnpi_image);
+            mLnpiTextView = itemView.findViewById(R.id.lnpi_text);
+            mLnpiCardView = itemView.findViewById(R.id.lnpi_cardview);
+
+        }
+    }
+}
